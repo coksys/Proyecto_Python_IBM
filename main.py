9b5111ee5706db5452e0e16bd8ab90e2c5cfcc22
Elevathion <coksys.micancion@gmail.com>
2024-11-19 23:10:40 +0100

    Inicio

---
+++ main.py
@@ -0,0 +1,9 @@
+class Producto:
+    def __init__(self, nombre, categoria, precio, cantidad):
+        self.__nombre = nombre
+        self.__categoria = categoria
+        self.__precio = precio
+        self.__cantidad = cantidad
+
+    
+
